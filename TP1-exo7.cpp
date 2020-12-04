9e96f51a5af48206ea05d3b0f8129330f37efdf5
Solem <solem.tchangai@orange.fr>
2020-12-04 17:28:38 +0100

    changement essai 2

--- TP1-exo7.cpp
+++ TP1-exo7.cpp
@@ -7,14 +7,6 @@ int main(int argc, char** argv){
 	float floatPositif;
 	int intPositif;
 	char more = 'y';
-	for(int count = 0; count<10 && more == 'y'; count++)
-	{
-		cout << "saisir un nombre à virgule positif : ";
-		cin >> floatPositif;
-		intPositif = round(floatPositif);
-		cout << intPositif << "\n";
-		cout << "more ? y/n ";
-		cin >> more;
-	}
+
 
 }
